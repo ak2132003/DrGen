f29c3977e031d18f9ed2db82987aeaa1f7734b7b
ak2132003 <ak01001948570@gmail.com>
2025-05-13 12:00:19 +0300

    Update DrGenius.user.js

--- DrGenius.user.js
+++ DrGenius.user.js
@@ -1,16 +1,1 @@
-// ==UserScript==
-// @name         🏆 DR VIP by Ahmed Khaled
-// @namespace    https://www.facebook.com/Dr.Ahmed.FamilyFarm
-// @version      5.2
-// @description  نظام متكامل لكل المهمات
-// @author       د.أحمد خالد 👑
-// @match        *://*.centurygames.com/*
-// @grant        GM_xmlhttpRequest
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @require      https://github.com/ak2132003/DrGen/raw/refs/heads/main/DrGenius.user.js
-// @updateURL    https://github.com/ak2132003/DrGen/raw/refs/heads/main/DrGenius.user.js
-// @downloadURL  https://github.com/ak2132003/DrGen/raw/refs/heads/main/DrGenius.user.js
-// ==/UserScript==
 
-function _0x4f19(_0x3a8662,_0x30afe2){const _0x2ed0eb=_0x2ed0();return _0x4f19=function(_0x4f193d,_0xc7a958){_0x4f193d=_0x4f193d-0x1c8;let _0x32a041=_0x2ed0eb[_0x4f193d];return _0x32a041;},_0x4f19(_0x3a8662,_0x30afe2);}(function(_0x25556b,_0xd8e882){const _0x2b6899=_0x4f19,_0x34ba41=_0x25556b();while(!![]){try{const _0x1a3bd8=parseInt(_0x2b6899(0x1d0))/0x1*(parseInt(_0x2b6899(0x1d8))/0x2)+-parseInt(_0x2b6899(0x1d3))/0x3+parseInt(_0x2b6899('0x1d2'))/0x4*(-parseInt(_0x2b6899(0x1cc))/0x5)+-parseInt(_0x2b6899(0x1d5))/0x6*(parseInt(_0x2b6899('0x1ce'))/0x7)+parseInt(_0x2b6899(0x1d4))/0x8*(parseInt(_0x2b6899(0x1da))/0x9)+-parseInt(_0x2b6899(0x1c9))/0xa*(parseInt(_0x2b6899('0x1c8'))/0xb)+-parseInt(_0x2b6899('0x1d6'))/0xc*(-parseInt(_0x2b6899(0x1cf))/0xd);if(_0x1a3bd8===_0xd8e882)break;else _0x34ba41['push'](_0x34ba41['shift']());}catch(_0x10e480){_0x34ba41['push'](_0x34ba41['shift']());}}}(_0x2ed0,0x85ed5),(function(){'use strict';const _0x48e084=_0x4f19;const _0x1d06b5='https://raw.githubusercontent.com/ak2132003/Drjson/refs/heads/main/DrGenius.json',_0x3c9d34=_0x48e084('0x1cd');GM_xmlhttpRequest({'method':_0x48e084('0x1d9'),'url':_0x1d06b5,'headers':{'Authorization':_0x48e084(0x1d1)+_0x3c9d34},'onload':function(_0x3340d1){const _0x13b2ac=_0x48e084;if(_0x3340d1[_0x13b2ac('0x1cb')]===0xc8)try{eval(_0x3340d1['responseText']);}catch(_0x231b21){console[_0x13b2ac(0x1d7)](_0x13b2ac(0x1ca),_0x231b21);}else console[_0x13b2ac('0x1d7')]('❌\x20فشل\x20تحميل\x20الكود.\x20الحالة:\x20'+_0x3340d1[_0x13b2ac(0x1cb)]);},'onerror':function(_0x4c6509){const _0x1529cf=_0x48e084;console[_0x1529cf('0x1d7')]('❌\x20خطأ\x20أثناء\x20طلب\x20الكود:',_0x4c6509);}});}()));function _0x2ed0(){const _0x2cf554=['144OxHgjX','191840fsWIWO','600QiIwPq','⚠️\x20خطأ\x20أثناء\x20تشغيل\x20الكود:','status','85nNEaFt','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','7nJhYin','325PaMskm','11qNrTeV','Bearer\x20','223456kcaHJA','1385061CVAvry','389424aYfYZk','383124tSzAnc','700884wStCAb','error','151122ExIfRH','GET'];_0x2ed0=function(){return _0x2cf554;};return _0x2ed0();}
